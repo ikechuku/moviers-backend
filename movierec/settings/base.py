37ebf52cc941612fb5cf20228865e78a1eaa2d08
ikechuku <rockstarik2@gmail.com>
2019-11-29 11:02:13 +0100

    env unable to find secret key

--- movierec/settings/base.py
+++ movierec/settings/base.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'fb307_f*^21$@h2_sld5773%7(1prhghnjyov)@mmhyzk8%ph('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
